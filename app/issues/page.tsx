1b15d64a0bb0f17b959ac473bc0de1d759bca474
Jarocha75 <jarocha75@gmail.com>
2025-10-28 20:26:11 +0100

    Setting up Redix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button, Flex, Text } from "@radix-ui/themes";
 
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Theme</Text>
+      <Button>New Issue</Button>
+    </Flex>
+  );
 };
 
 export default IssuePage;
