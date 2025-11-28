4758835555e58d5b7b3a6b687b853b771706b0bd
Jarocha75 <jarocha75@gmail.com>
2025-11-28 13:39:07 +0100

    add new issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -20,7 +20,7 @@ const IssuePage = async ({ searchParams }: Props) => {
     value: keyof Issue;
     className?: string;
   }[] = [
-    { label: "Issue", value: "title" },
+    { label: "Issues", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
